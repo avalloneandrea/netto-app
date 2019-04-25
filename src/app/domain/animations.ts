1af63e0d767641bea4c42b2edc9c6622ef6c2f3e
Andrea Avallone <avalloneandrea@gmail.com>
2019-04-25 10:23:07 +0200

    Fix animation

--- src/app/domain/animations.ts
+++ src/app/domain/animations.ts
@@ -1,8 +1,8 @@
 import { animate, animation, stagger, style } from '@angular/animations';
 
 const fadeIn = animation([
-  style({ opacity: 0, transform: 'translateY(-100px)' }),
-  stagger(-30, [
+  style({ opacity: 0, transform: 'translateY(100px)' }),
+  stagger('30ms', [
     animate('500ms cubic-bezier(0.35, 0, 0.25, 1)',
       style({ opacity: 1, transform: 'none' }))
   ])
