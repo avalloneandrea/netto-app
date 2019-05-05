75b631793674b90955bf15309a83a1a7fd1cb13e
Andrea Avallone <avalloneandrea@gmail.com>
2019-05-05 14:35:31 +0200

    Update Protractor tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,28 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Neat App', () => {
+
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
-  });
-
-  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to neat-app!');
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+  it('should display the header', () => {
+    expect(page.getHeader().isPresent).toBeTruthy();
   });
+
+  it('should display the body', () => {
+    expect(page.getBody().isPresent).toBeTruthy();
+  });
+
+  it('should display the footer', () => {
+    expect(page.getFooter().isPresent).toBeTruthy();
+  });
+
+  it('should display the localized submit button', () => {
+    expect(page.getButton().getText()).toContain('Compute paycheck');
+  });
+
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,25 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
+
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  getHeader() {
+    return element(by.css('.navbar'));
   }
+
+  getBody() {
+    return element(by.css('.hero'));
+  }
+
+  getFooter() {
+    return element(by.css('.footer'));
+  }
+
+  getButton() {
+    return element(by.css('.button'));
+  }
+
 }
